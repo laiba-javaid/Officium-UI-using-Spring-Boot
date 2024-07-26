99b7273ba892de42ac6344e1152510e4ca40587d
laiba-javaid <laibajavaid423@gmail.com>
2024-07-26 22:10:06 +0500

    Login Application using spring boot

---
+++ src/main/java/com/loginapplication/loginapplication/Controller/LoginController.java
@@ -0,0 +1,23 @@
+package com.loginapplication.loginapplication.Controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@Controller
+public class LoginController 
+{
+@PostMapping("/login")
+public String login(@RequestParam String username, @RequestParam String password, Model model)
+{
+    if ("admin".equals(username) && "password".equals(password)) {
+        model.addAttribute("username", username);
+        return "welcome"; 
+    } else {
+        model.addAttribute("error", "Invalid username or password");
+        return "index"; 
+    }
+}
+
+}
