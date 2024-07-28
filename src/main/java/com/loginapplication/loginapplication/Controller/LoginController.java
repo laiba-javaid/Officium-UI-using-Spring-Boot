d6da13ebee2060191c9a1ca94a063090439c5636
laiba-javaid <laibajavaid423@gmail.com>
2024-07-28 22:41:22 +0500

    updated welcome page

--- src/main/java/com/loginapplication/loginapplication/Controller/LoginController.java
+++ src/main/java/com/loginapplication/loginapplication/Controller/LoginController.java
@@ -1,23 +1,21 @@
 package com.loginapplication.loginapplication.Controller;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-
 @Controller
-public class LoginController 
-{
-@PostMapping("/login")
-public String login(@RequestParam String username, @RequestParam String password, Model model)
-{
-    if ("admin".equals(username) && "password".equals(password)) {
-        model.addAttribute("username", username);
-        return "welcome"; 
-    } else {
-        model.addAttribute("error", "Invalid username or password");
-        return "index"; 
+public class LoginController {
+    @PostMapping("/login")
+    public String login(@RequestParam String username, @RequestParam String password, Model model) {
+        if ("Laiba".equals(username) && "123".equals(password)) {
+            model.addAttribute("username", username);
+            return "welcome";
+        } else {
+            model.addAttribute("error", "Invalid username or password");
+            return "index";
+        }
     }
-}
 
 }
